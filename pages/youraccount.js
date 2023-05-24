467055b6deac34ffb40c347c56d0e92629c49e23
boku <zaregototsukai13@gmail.com>
2023-05-24 08:57:36 +0530

    fix2

--- pages/youraccount.js
+++ pages/youraccount.js
@@ -35,7 +35,7 @@ const YourAccount = () => {
     <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
       <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">Your Account
       </h1>
-      <p className="mb-8 leading-relaxed">Email: {session.user.email}</p>
+      <p className="mb-8 leading-relaxed">Email: b22ee036@iitj.ac.in</p>
       <div className="flex justify-center">
         <button className="inline-flex text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">Add more info</button>
       </div>
