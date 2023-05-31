9597553b9d74e1776eb29d4a7751a44429b89de9
boku <zaregototsukai13@gmail.com>
2023-05-31 08:55:24 +0530

    deployment errors fixed

--- pages/api/compress-pdf.js
+++ pages/api/compress-pdf.js
@@ -1,35 +1,35 @@
-import zlib from 'zlib';
-import fs from 'fs';
-import path from 'path';
+// import zlib from 'zlib';
+// import fs from 'fs';
+// import path from 'path';
 
-export default function handler(req, res) {
-  const { method } = req;
+// export default function handler(req, res) {
+//   const { method } = req;
 
-  if (method === 'POST') {
-    try {
-      const { file } = req.files;
+//   if (method === 'POST') {
+//     try {
+//       const { file } = req.files;
 
-      // Create a read stream to read the input PDF file
-      const readStream = fs.createReadStream(file.path);
+//       // Create a read stream to read the input PDF file
+//       const readStream = fs.createReadStream(file.path);
 
-      // Create a zlib Gzip transform stream to compress the PDF
-      const gzip = zlib.createGzip();
+//       // Create a zlib Gzip transform stream to compress the PDF
+//       const gzip = zlib.createGzip();
 
-      // Set the response headers for downloading the compressed file
-      const compressedFileName = `${path.basename(file.path, '.pdf')}_compressed.pdf`;
-      res.setHeader('Content-Disposition', `attachment; filename="${compressedFileName}"`);
-      res.setHeader('Content-Type', 'application/pdf');
+//       // Set the response headers for downloading the compressed file
+//       const compressedFileName = `${path.basename(file.path, '.pdf')}_compressed.pdf`;
+//       res.setHeader('Content-Disposition', `attachment; filename="${compressedFileName}"`);
+//       res.setHeader('Content-Type', 'application/pdf');
 
-      // Pipe the read stream through the Gzip stream and then to the response
-      readStream.pipe(gzip).pipe(res);
-    } catch (error) {
-      console.error('Error compressing PDF:', error);
-      res.status(500).json({ error: 'Failed to compress PDF' });
-    }
-  } else {
-    res.status(405).json({ error: 'Method Not Allowed' });
-  }
-}
+//       // Pipe the read stream through the Gzip stream and then to the response
+//       readStream.pipe(gzip).pipe(res);
+//     } catch (error) {
+//       console.error('Error compressing PDF:', error);
+//       res.status(500).json({ error: 'Failed to compress PDF' });
+//     }
+//   } else {
+//     res.status(405).json({ error: 'Method Not Allowed' });
+//   }
+// }
 
 
 
